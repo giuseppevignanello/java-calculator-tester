bb80ece7e19aabeb4e63590d02cc24b2ac638ad8
giuseppevignanello <peppe.vignanello@gmail.com>
2023-09-29 12:32:51 +0200

    add calculator

---
+++ src/main/java/org/java/Calculator.java
@@ -0,0 +1,26 @@
+package org.java;
+
+public class Calculator {
+	
+	public Calculator() {
+		
+	}
+	
+	float add (float num1, float num2) {
+		float result = num1 + num2;
+		return result;
+	} 
+	
+	float subtract(float num1, float num2) {
+		float result = num1 - num2; 
+		return result;
+	}
+	float divide(float num1, float num2) {
+		float result = num1 * num2;
+		return result;
+	}
+	float multiply(float num1, float num2) {
+		float result = num1 / num2;
+		return result;
+	}
+}
